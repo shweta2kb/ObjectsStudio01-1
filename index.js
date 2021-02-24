ce6e616c302ca55c6d626852da3c7d9965d64704
shweta2kb <shweta2kb@gmail.com>
2021-02-24 19:17:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// Code your selectRandomEntry function here:
+function selectRandomEntry(arr){
+   let index =  Math.floor(Math.random()*arr.length);
+   return arr[index];
+}
+let idNumbers = [291, 414, 503, 599, 796, 890];
+
+for (i=0; i < 6; i++){
+   console.log(selectRandomEntry(idNumbers));
+}
+// Code your buildCrewArray function here:
+function buildCrewArray(arr1,obj){
+  arr1=selectRandomEntry(arr);
+  obj = arrayName.includes(item)
+}
+
+// Here are the candidates and the 'animals' array:
+let candidateA = {
+  'name':'Gordon Shumway',
+  'species':'alf',
+  'mass':90,
+  'o2Used':function(hrs){return 0.035*hrs},
+  'astronautID':414
+};
+let candidateB = {
+  'name':'Lassie',
+  'species':'dog',
+  'mass':19.1,
+  'o2Used':function(hrs){return 0.030*hrs},
+  'astronautID':503
+};
+let candidateC = {
+  'name':'Jonsey',
+  'species':'cat',
+  'mass':3.6,
+  'o2Used':function(hrs){return 0.022*hrs},
+  'astronautID':796
+};
+let candidateD = {
+  'name':'Paddington',
+  'species':'bear',
+  'mass':31.8,
+  'o2Used':function(hrs){return 0.047*hrs},
+  'astronautID':291
+};
+let candidateE = {
+  'name':'Pete',
+  'species':'tortoise',
+  'mass':417,
+  'o2Used':function(hrs){return 0.010*hrs},
+  'astronautID':599
+};
+let candidateF = {
+  'name':'Hugs',
+  'species':'ball python',
+  'mass':2.3,
+  'o2Used':function(hrs){return 0.018*hrs},
+  'astronautID':890
+};
+let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+
+for (item in animals) {
+   console.log(animals[item]);
+}
+// Code your template literal and console.log statements:
